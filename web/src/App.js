e88cfae5293e2d83c0049445af5b9bf1ab3a6e24
plougsgaard <thomas@plougsgaard.dk>
2016-08-26 22:49:33 +0200

    (´･ω･`)

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+  <div className='align-center'>
+    <div className='header'>
+      <h2>Playground</h2>
+    </div>
+    <p className='section'>
+      Here shall be things of plenty.
+    </p>
+  </div>
+)
