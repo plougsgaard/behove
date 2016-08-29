5e2f6ce68653f8b413c7a579faff10591bcca039
plougsgaard <thomas@plougsgaard.dk>
2016-08-29 20:37:50 +0200

    Rewrite network service around wrapped fetch.

--- src/services/network.js
+++ src/services/network.js
@@ -24,20 +24,7 @@ export const getBadRequest = () => ({
   }
 })
 
-const _parseUrl = (url) => {
-  if (!url) {
-    throw new Error(`Missing URL.`)
-  }
-  if (url.indexOf('http') === 0) {
-    return url
-  }
-  if (url[0] === '/') {
-    return `${PATH_API_BASE}${url}`
-  }
-  return `${PATH_API_BASE}/${url}`
-}
-
-const _parseOptions = (options = {}) => {
+const getOptions = (options = {}) => {
   if (options.body) {
     options.body = JSON.stringify(options.body)
     options.headers = {
@@ -48,9 +35,58 @@ const _parseOptions = (options = {}) => {
   return options
 }
 
+const wrapFetch = async (url, options) => {
+  const response = await fetch(url, options)
+  const json = await response.json()
+  if (json.ok) {
+    return json
+  }
+  throw { error: json }
+}
+
+const decoratePath = (url) => `${PATH_API_BASE}/${url}`
+
+const decorateOptions = (method = 'get', body, headers) => body
+  ? {
+    method,
+    body: JSON.stringify(body),
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    }
+  }
+  : {
+    method
+  }
+
+export const getRequest = async (url, body, headers) =>
+  await wrapFetch(decoratePath(url), decorateOptions('get', body, headers))
+
+export const postRequest = async (url, body, headers) =>
+  await wrapFetch(decoratePath(url), decorateOptions('post', body, headers))
+
+export const putRequest = async (url, body, headers) =>
+  await wrapFetch(decoratePath(url), decorateOptions('put', body, headers))
+
+export const deleteRequest = async (url, body, headers) =>
+  await wrapFetch(decoratePath(url), decorateOptions('delete', body, headers))
+
+// export const postRequest = async (url, options) => {
+//   try {
+//     const normalResponse = await fetch(decoratePath(url), getOptions(options))
+//     console.log('postRequest:normalResponse', normalResponse)
+//     if (normalResponse.status >= 400) {
+//       throw Boom.create(normalResponse.status, normalResponse.statusText)
+//     }
+//   } catch (errorResponse) {
+//     console.log('postRequest:errorResponse', errorResponse)
+//     throw Error({ error: errorResponse })
+//   }
+// }
+
 export const httpRequest = (url, options) => {
   return new Promise((resolve, reject) => {
-    fetch(_parseUrl(url), _parseOptions(options)).then(
+    fetch(decoratePath(url), getOptions(options)).then(
       async (response) => {
         if (response.status >= 400) {
           try {
--- web/src/Main.js
+++ web/src/Main.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 import { connect } from 'react-redux'
 
+import { delay } from 'redux-saga'
+
 import { actions } from './app/reducers/auth'
+import { postRequest } from './app/services/network'
+
+async function clickHandler (e) {
+  try {
+    const response = await postRequest()
+    await delay(2000)
+    console.log('clickHandler response:', response)
+  } catch (error) {
+    console.warn(error)
+  }
+}
 
 const Main = ({ auth, dispatch }) => (
   <div className='align-center'>
@@ -12,6 +25,10 @@ const Main = ({ auth, dispatch }) => (
     <p className='section'>
       Here shall be things of plenty.
     </p>
+    <div className='section'>
+      <h1>Network</h1>
+      <button onClick={clickHandler}>Login with postRequest</button>
+    </div>
     <div className='section'>
       <h1>Auth</h1>
       <p>{JSON.stringify(auth)}</p>
