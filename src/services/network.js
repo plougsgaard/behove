73877ddc10fe9648437300d69ed8c04c724e8430
plougsgaard <thomas@plougsgaard.dk>
2016-08-31 20:45:11 +0200

    Authenticated network request functions.

--- src/services/network.js
+++ src/services/network.js
@@ -2,6 +2,12 @@
 
 import { PATH_API_BASE } from '../config'
 
+/**
+ * Wrapper around `fetch`.
+ *
+ * Throws if the `response` is unsuccessful ~ ![200;300)
+ * Also throws (implicitly) if there isn't a JSON body to parse.
+ */
 const wrapFetch = async (url, options) => {
   const response = await fetch(url, options)
   const json = await response.json()
@@ -13,18 +19,20 @@ const wrapFetch = async (url, options) => {
 
 const decoratePath = (url) => `${PATH_API_BASE}/${url}`
 
-const decorateOptions = (method = 'get', body, headers) => body
-  ? {
-    method,
-    body: JSON.stringify(body),
-    headers: {
-      ...headers,
-      'Content-Type': 'application/json'
-    }
-  }
-  : {
-    method
-  }
+/**
+ * Builds options for the `fetch` function.
+ *
+ * - Add content type `application/json`
+ * - Stringify the `body` if present
+ */
+const decorateOptions = (method = 'get', body, headers) => ({
+  method,
+  headers: {
+    ...headers,
+    'Content-Type': 'application/json'
+  },
+  ...( body ? { body: JSON.stringify(body) } : {})
+})
 
 export const getRequest = async (url, body, headers) =>
   await wrapFetch(decoratePath(url), decorateOptions('get', body, headers))
@@ -37,3 +45,18 @@ export const putRequest = async (url, body, headers) =>
 
 export const deleteRequest = async (url, body, headers) =>
   await wrapFetch(decoratePath(url), decorateOptions('delete', body, headers))
+
+/**
+ * Helper function to build authenticated request functions.
+ *
+ * @param  {function} asyncRequestFunction - request function
+ * @param  {string} token - API token
+ * @return {function} request function embedding the token as a header
+ */
+const authenticated = (asyncRequestFunction, token) => async (url, body, headers) =>
+  await asyncRequestFunction(url, body, { ...headers, token })
+
+export const authenticatedGetRequest = (token) => authenticated(getRequest, token)
+export const authenticatedPostRequest = (token) => authenticated(postRequest, token)
+export const authenticatedPutRequest = (token) => authenticated(putRequest, token)
+export const authenticatedDeleteRequest = (token) => authenticated(deleteRequest, token)
