33f0b6552d2440cdcef107d1fdfb56853f3cc3c0
plougsgaard <thomas@plougsgaard.dk>
2016-09-07 22:33:54 +0200

    Refactor all startup into Root.

--- index.android.js
+++ index.android.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import Root from './src/Root'
-import configureStore from './src/redux/store'
-
-const store = configureStore({
-  development: (typeof __DEV__ !== 'undefined')
-})
-
-const PreRoot = () => (
-  <Root store={store} />
-)
-
-AppRegistry.registerComponent('behove', () => PreRoot)
+require('./src/Root')()
--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { AppRegistry } from 'react-native'
 import { View, StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
+import configureStore from './redux/store'
 import styles from './Root.styles.js'
 
 import NavigationRouter from './navigation/NavigationRouter'
 
-export default ({ store }) => (
-  <Provider store={store}>
-    <View style={styles.applicationView}>
-      <StatusBar
-        barStyle='light-content'
-      />
-      <NavigationRouter />
-    </View>
-  </Provider>
-)
+const store = configureStore({
+  development: (typeof __DEV__ !== 'undefined')
+})
+
+const Root = React.createClass({
+  displayName: 'Root',
+  render: function () {
+    return (
+      <Provider store={store}>
+        <View style={styles.applicationView}>
+          <StatusBar
+            barStyle='light-content'
+          />
+          <NavigationRouter />
+        </View>
+      </Provider>
+    )
+  }
+})
+
+module.exports = () => AppRegistry.registerComponent('behove', () => Root)
